d28efc14467ee967a7148fdbab39031bb7657168
Daniel Acevedo <daniel18acevedo@gmail.com>
2023-10-08 11:16:46 -0300

    Adding utility

---
+++ CQ.Utility/Db.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQ.Utility
+{
+    public static class Db
+    {
+        public static string NewId()
+        {
+            return Guid.NewGuid().ToString().Replace("-", "");
+        }
+
+        public static void ThrowIdHasNotValidFormat(string id)
+        {
+            var isIdInvalid = !Guid.TryParse((id.Insert(8, "-").Insert(13, "-").Insert(18, "-").Insert(21, "-").Insert(23, "-")), out Guid guidId);
+            if (isIdInvalid)
+            {
+                throw new ArgumentException("The id is invalid");
+            }
+        }
+
+        public static void ThrowIdHasNotValidFormat<TException>(string id)
+            where TException : Exception, new()
+        {
+            var isIdInvalid = !Guid.TryParse((id.Insert(8, "-").Insert(13, "-").Insert(18, "-").Insert(21, "-").Insert(23, "-")), out Guid guidId);
+            if (isIdInvalid)
+            {
+                throw new TException();
+            }
+        }
+    }
+}
