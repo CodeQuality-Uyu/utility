f38c46ecf801b5d4839011f2c1acf26e82d61d61
Daniel Acevedo <daniel18acevedo@gmail.com>
2023-10-17 20:34:04 -0300

    Adding test

--- CQ.Utility.Tests/HttpClientAdapterTest.cs
+++ CQ.Utility.Tests/HttpClientAdapterTest.cs
@@ -9,5 +9,33 @@ namespace CQ.Utility.Tests
     [TestClass]
     public class HttpClientAdapterTest
     {
+        private HttpClientAdapter httpClientAdapter;
+
+        [TestInitialize]
+        public void OnInit()
+        {
+            httpClientAdapter = new HttpClientAdapter("https://identitytoolkit.googleapis.com", new List<(string, string)> { ("Referer", "localhost:7049") });
+        }
+
+        [TestMethod]
+        public async Task WhenEmailNotFound_ThenErrorIsReturned()
+        {
+            await httpClientAdapter.PostAsync<SuccessBody, ErrorBody>("v1/accounts:signInWithCustomToken?key=", new { email = "some@email.com", password = "123456" }).ConfigureAwait(false);
+        }
+    }
+
+    internal sealed record class SuccessBody
+    {
+        public string Name { get; set; }
+    }
+
+    internal sealed record class ErrorBody
+    {
+        public Error Error { get; set; }
+    }
+
+    internal sealed record class Error
+    {
+        public string Message { get; set; }
     }
 }
