3831601b459765c11c8bd630ef95feb0739f3627
Daniel Acevedo <daniel18acevedo@gmail.com>
2023-10-31 16:11:42 -0300

    Refactor

---
+++ CQ.Utility/ConnectionRefusedException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQ.Utility
+{
+    public class ConnectionRefusedException : Exception
+    {
+        public readonly string Connection;
+
+        public ConnectionRefusedException(string connection)
+        {
+            Connection = connection;
+        }
+    }
+}
---
+++ CQ.Utility/Header.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQ.Utility
+{
+    public readonly record struct Header(string Name, string Value);
+}
